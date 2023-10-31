b100a87b0a69623a18a4256f68c58163bf2be686
Denis Tereshchenko <113333350+dotereschenko@users.noreply.github.com>
2023-10-31 15:48:04 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 import setuptools
 
 setuptools.setup(
-    name='rl_course_pnp',
+    name='Predators-and-Prey',
     version='0.1',
     author='dotereschenko',
     description='Predators-and-Prey',
     long_description_content_type="text/markdown",
-    url='https://github.com/dotereschenko/Predators-and-Prey/tree/stage1',
+    url='https://github.com/dotereschenko/Predators-and-Prey',
     license='MIT',
     packages=setuptools.find_packages(exclude=['tests']),
     install_requires=['opencv-python',
